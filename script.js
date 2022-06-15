50e2243307157e861a4dd23201cd57af88c28539
Soltys69 <soltysnowicki@gmail.com>
2022-06-15 14:32:33 +0200

    HTML, CSS & JS update
    Added JS script
    corrected CSS and HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+// DOM elements selector
+const questions = document.querySelectorAll(".question");
+const description = document.querySelectorAll(".desc");
+const arrow = document.querySelectorAll(".arrow");
+
+// For loop through elements and adding event listener to them
+questions.forEach((question) =>
+  question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.toggle("active");
+    } else {
+      questions.forEach((question) =>
+        question.parentNode.classList.remove("active")
+      );
+      question.parentNode.classList.add("active");
+    }
+  })
+);
