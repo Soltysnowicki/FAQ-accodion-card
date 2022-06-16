2bf2996f75de6e44a91f2cee2294cc3cc421dc15
Soltys69 <soltysnowicki@gmail.com>
2022-06-16 10:38:36 +0200

    CSS update
    CSS correction

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const arrow = document.querySelectorAll(".arrow");
 
 // For loop through elements and adding event listener to them
 questions.forEach((question) =>
-  question.addEventListener("click", () => {
-    if (question.parentNode.classList.contains("active")) {
-      question.parentNode.classList.toggle("active");
-    } else {
-      questions.forEach((question) =>
-        question.parentNode.classList.remove("active")
-      );
-      question.parentNode.classList.add("active");
+  question.addEventListener("click", () =>
+    //adding and removing active class to elements
+    {
+      if (question.parentNode.classList.contains("active")) {
+        question.parentNode.classList.toggle("active");
+      } else {
+        questions.forEach((question) =>
+          question.parentNode.classList.remove("active")
+        );
+        question.parentNode.classList.add("active");
+      }
     }
-  })
+  )
 );
